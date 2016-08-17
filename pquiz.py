b940d5d7b87228a3e878492628cea14b88b42a85
sharktamer <donohoe.shane@gmail.com>
2016-08-17 17:51:31 +0100

    Cleanup and score counting

--- pquiz.py
+++ pquiz.py
@@ -4,28 +4,17 @@ import json
 from sopel.module import rule, commands
 import os.path
 
-basepath = os.path.dirname(__file__)
-jsonfile = os.path.abspath(os.path.join(basepath, 'mons.json'))
-with open(jsonfile) as f:
-    j = json.load(f)
 
+def setup(bot):
+    bot.memory['pquiz'] = {
+        'active': False,
+        'scores': {}
+    }
 
-@rule('[^\.].*')
-def panswer(bot, trigger):
-    if not bot.memory.contains('pq_active') or not bot.memory['pq_active']:
-        return
-
-    removed = False
-    for i in trigger.args[1].split():
-        if i.lower() in bot.memory['pj']:
-            removed = True
-            bot.memory['pj'].remove(i.lower())
-    if removed:
-        remaining = bot.memory['pj_size'] - len(bot.memory['pj'])
-        bot.say('pquiz: {}/{}'.format(remaining, bot.memory['pj_size']))
-        if not len(bot.memory['pj']):
-            bot.say('You named all the pokemon!')
-            bot.memory['pq_active'] = False
+    basepath = os.path.dirname(__file__)
+    jsonfile = os.path.abspath(os.path.join(basepath, 'mons.json'))
+    with open(jsonfile) as f:
+        bot.memory['pquiz']['list'] = json.load(f)
 
 
 @commands('pquiz')
@@ -36,6 +25,8 @@ def pquiz(bot, trigger):
         bot.say('Error: arguments must be integer values')
         return
 
+    j = bot.memory['pquiz']['list']
+
     if len(args) == 0:
         pj = list(j.values())
         bot.say('Starting pquiz with all mons')
@@ -45,6 +36,32 @@ def pquiz(bot, trigger):
     else:
         pj = [j[i] for i in j if int(i) >= args[0] and int(i) <= args[1]]
         bot.say('Starting pquiz with mons {}-{}'.format(*args[:2]))
-    bot.memory['pj'] = pj
-    bot.memory['pj_size'] = len(pj)
-    bot.memory['pq_active'] = True
+    bot.memory['pquiz']['game_list'] = pj
+    bot.memory['pquiz']['game_size'] = len(pj)
+    bot.memory['pquiz']['active'] = True
+
+
+@rule('[^\.].*')
+def panswer(bot, trigger):
+    if not bot.memory['pquiz']['active']:
+        return
+
+    removed = False
+    for i in trigger.args[1].split():
+        if i.lower() in bot.memory['pquiz']['game_list']:
+            removed = True
+            bot.memory['pquiz']['game_list'].remove(i.lower())
+            if trigger.nick in bot.memory['pquiz']['scores']:
+                bot.memory['pquiz']['scores'][trigger.nick] += 1
+            else:
+                bot.memory['pquiz']['scores'][trigger.nick] = 1
+    if removed:
+        size = bot.memory['pquiz']['game_size']
+        remaining = size - len(bot.memory['pquiz']['game_list'])
+        bot.say('pquiz: {}/{}'.format(remaining, size))
+        if not len(bot.memory['pquiz']['game_list']):
+            bot.say('You named all the pokemon!')
+            bot.say('Scores:')
+            for i in bot.memory['pquiz']['scores']:
+                bot.say('{}: {}'.format(i, bot.memory['pquiz']['scores'][i]))
+            bot.memory['pquiz']['active'] = False
